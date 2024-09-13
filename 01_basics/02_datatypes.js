79ff9555b8578ef4d9533294011f479dcf313960
mahipatel24 <125684792+mahipatel24@users.noreply.github.com>
2024-09-13 09:23:39 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+// alert() // we are using nodejs , not  browser
+
+console.log(3+3)
+
+console.log("Mahi")
+
+let name = "mahi"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique mostly used Recatjs
+// object
+
+console.log(typeof null); // type- object
+console.log(typeof undefined); // type - undefined
