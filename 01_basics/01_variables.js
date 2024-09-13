c77eca328ab46821b8d6e452ca13694547e40d31
mahipatel24 <125684792+mahipatel24@users.noreply.github.com>
2024-09-13 07:19:28 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "maahi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // Not Allowed
+accountEmail = "mp@mp.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
