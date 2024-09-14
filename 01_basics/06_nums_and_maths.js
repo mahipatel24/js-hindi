a7dfb71b44a0a5c1dd89166a70645e2929f7ff94
mahipatel24 <125684792+mahipatel24@users.noreply.github.com>
2024-09-14 11:33:39 +0000

    Math And Num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const  score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//****************************Maths****************************** */
+
+// console.log(Math);
+// console.log(Math.abs(4)); // this function converted into negative value into positive and positive into same as positive
+// console.log(Math.round(4.88)); // this function is the value converted in roundoff (ex value : 5.86 O/p Value : 6)
+// console.log(Math.ceil(4.2)); // (ex value : 4.2 O/p Value : 5)
+// console.log(Math.floor(4.2));//(ex value : 4.2 O/p Value : 4)
+// console.log(Math.min(4, 5, 2, 9));//pick min value in array
+// console.log(Math.Math(4, 5, 2, 9));//pick max value in array
+
+console.log(Math.random()); // math.random value is the between 1 and 0
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*6)+1);// make a dice
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)))+min)
+
+
+
+
+
